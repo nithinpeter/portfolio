24b8d577974fce56444415c4f3e4a1b0dddcb819
Nithin Peter <nithinpeter7@gmail.com>
2016-08-25 06:13:18 +1000

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>NITHIN PETER</h2>
+        </div>
+        <p className="App-intro">
+          Front-end Developer
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
